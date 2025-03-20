200c85ce85bc2fef965f4ddce3b98003f537897b
hellohowareyouhaveaniceday <liboli.lilibo@gmail.com>
2025-03-19 22:11:40 -0400

    ugh

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { supabase } from './client'
 import { Session } from '@supabase/supabase-js'
 import Auth from './components/Auth'
 import Account from './components/Account'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import BabyTracker from './mini-apps/babytracker/BabyTrackerClient'
 import './App.css'
 
 function App() {
   const [session, setSession] = useState<Session | null>(null)
-  
+  const [app, setApp] = useState<string>('home')
+
   useEffect(() => {
     // Check for active session on component mount
     supabase.auth.getSession().then(({ data: { session } }) => {
@@ -31,7 +31,23 @@ function App() {
       {!session ? (
         <Auth />
       ) : (
-        <Account session={session} />
+        <div>
+
+          <button onClick={() => setApp('home')}>Home</button>
+          <button onClick={() => setApp('babytracker')}>Baby Tracker</button>
+          <div>
+            <Account session={session} />
+          </div>
+
+          {app === 'home' && <div>Home</div>}
+          {app === 'babytracker' && (
+            <div>
+              <h1>Baby Tracker</h1>
+              <BabyTracker />
+            </div>
+          )}
+
+        </div>
       )}
     </div>
   )
